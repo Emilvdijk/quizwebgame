2cb09f5f84992a13944b506008b9689c36bfd0eb
emilvdijk <emilvdijk@gmail.com>
2024-07-23 11:19:35 +0200

    added simple test

---
+++ src/test/java/nl/emilvdijk/quizwebgame/config/WebMvcConfigTest.java
@@ -0,0 +1,27 @@
+package nl.emilvdijk.quizwebgame.config;
+
+import nl.emilvdijk.quizwebgame.QuizwebgameApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(classes = QuizwebgameApplication.class)
+class WebMvcConfigTest {
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  @Test
+  void passwordEncoder() {
+    String result = passwordEncoder.encode("myPassword");
+//    System.out.println(result);
+    assertTrue(passwordEncoder.matches("myPassword", result));
+
+
+  }
+}
