15627cbe7c384a2a30400ade8992157f30a370c7
emilvdijk <emilvdijk@gmail.com>
2024-07-26 12:01:55 +0200

    added navbar and some security thingies

--- src/main/java/nl/emilvdijk/quizwebgame/config/WebMvcConfig.java
+++ src/main/java/nl/emilvdijk/quizwebgame/config/WebMvcConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig {
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-  }
+  //  @Bean
+  //  public PasswordEncoder passwordEncoder() {
+  //    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  //  }
 }
