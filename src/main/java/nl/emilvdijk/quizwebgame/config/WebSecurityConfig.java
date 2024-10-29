2e12a81f47213dd9193ae8b1bebee6336254e643
emilvdijk <emilvdijk@gmail.com>
2024-10-29 11:55:00 +0100

    added seperate SecurityfilterChains for h2 api and base

--- src/main/java/nl/emilvdijk/quizwebgame/config/WebSecurityConfig.java
+++ src/main/java/nl/emilvdijk/quizwebgame/config/WebSecurityConfig.java
@@ -4,18 +4,46 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
   @Bean
+  @Order(1)
+  SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
+    http.securityMatcher(AntPathRequestMatcher.antMatcher("/h2-console/**"))
+        .authorizeHttpRequests(request -> request.requestMatchers("/h2-console/**").permitAll())
+        .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+        .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin));
+    return http.build();
+  }
+
+  @Bean
+  @Order(2)
+  SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+    http.securityMatcher(AntPathRequestMatcher.antMatcher("/api/**"))
+        .authorizeHttpRequests(
+            request ->
+                request
+                    .requestMatchers("/api/questions/**")
+                    .hasRole("ADMIN")
+                    .anyRequest()
+                    .authenticated());
+    return http.build();
+  }
+
+  @Bean
+  @Order(Ordered.LOWEST_PRECEDENCE)
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(
             requests ->
@@ -25,16 +53,11 @@ public class WebSecurityConfig {
                     .permitAll()
                     .requestMatchers("/userPreferences")
                     .hasRole("USER")
-                    .requestMatchers("/h2-console/**")
-                    .hasRole("ADMIN")
                     .anyRequest()
                     .authenticated())
         .httpBasic(withDefaults())
         .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/", true).permitAll())
         .logout(logout -> logout.logoutSuccessUrl("/").permitAll());
-    //    FIXME remove after developing only h2 needs frameoptions same origin
-    http.headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin));
-    http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));
     return http.build();
   }
 
