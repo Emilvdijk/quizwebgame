33fb1a16390f71cb295a412f19780b409526acda
emilvdijk <emilvdijk@gmail.com>
2024-11-12 11:33:07 +0100

    added line to error message

--- src/main/java/nl/emilvdijk/quizwebgame/exceptions/ApiErrorException.java
+++ src/main/java/nl/emilvdijk/quizwebgame/exceptions/ApiErrorException.java
@@ -3,6 +3,6 @@ package nl.emilvdijk.quizwebgame.exceptions;
 public class ApiErrorException extends RuntimeException {
 
   public ApiErrorException(String message) {
-    super(message + " your settings have been reset");
+    super(message + " your settings have been reset, sorry for the inconvenience.");
   }
 }
