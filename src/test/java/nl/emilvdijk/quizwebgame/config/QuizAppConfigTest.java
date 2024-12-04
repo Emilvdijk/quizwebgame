afe4d6c2138804a599051a8907aab312c3adb184
emil <emilvdijk@gmail.com>
2024-12-04 17:32:16 +0100

    fixed bug in test

--- src/test/java/nl/emilvdijk/quizwebgame/config/QuizAppConfigTest.java
+++ src/test/java/nl/emilvdijk/quizwebgame/config/QuizAppConfigTest.java
@@ -2,6 +2,8 @@ package nl.emilvdijk.quizwebgame.config;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import nl.emilvdijk.quizwebgame.entity.MyUser;
 import nl.emilvdijk.quizwebgame.repository.UserRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,13 @@ class QuizAppConfigTest {
 
   @Test
   void myUserService() {
-    assertEquals(2, userRepo.findAll().size());
+    List<MyUser> userList = userRepo.findAll();
+    userList.forEach(
+        myUser ->
+            assertTrue(
+                myUser.getUsername().equals("admin")
+                    || myUser.getUsername().equals("user")
+                    || myUser.getUsername().equals("testUser")
+                    || myUser.getUsername().equals("testUsername")));
   }
 }
