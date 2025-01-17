062edc2bd4652647031683fa49da419196e791b4
emilvdijk <emilvdijk@gmail.com>
2025-01-17 11:04:30 +0100

    added commends

--- src/main/java/nl/emilvdijk/quizwebgame/controller/ErrorController.java
+++ src/main/java/nl/emilvdijk/quizwebgame/controller/ErrorController.java
@@ -3,16 +3,31 @@ package nl.emilvdijk.quizwebgame.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * base controller for errors.
+ *
+ * @author Emil van Dijk
+ */
 @Controller
 public class ErrorController {
 
-  @GetMapping("/error403")
-  public String error403() {
-    return "error403";
-  }
-
+  /**
+   * unauthenticated error page.
+   *
+   * @return error401 page
+   */
   @GetMapping("/error401")
   public String error401() {
     return "error401";
+  }
+
+  /**
+   * unauthorized/forbidden error page.
+   *
+   * @return error403 page
+   */
+  @GetMapping("/error403")
+  public String error403() {
+    return "error403";
   }
 }
